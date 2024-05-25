c2968302b5293e0b1c98894350c256e5c577615c
{Ben Cannon} <{BCannon1@sheffield.ac.uk}>
2024-05-25 20:46:42 +0100

    Made box links use react router link component

--- ben-cannon-portfolio/src/components/DescriptionBox.js
+++ ben-cannon-portfolio/src/components/DescriptionBox.js
@@ -1,9 +1,11 @@
+import { Link } from "react-router-dom";
+
 function DescriptionBox({title, content, link}) {
     return (
       <div>
         <h2 className='text-3xl font-bold font-mono'>{title}</h2>
         <p className='text-sm font-mono'>{content}</p>
-        {link && <a href={link}>click here</a>}
+        {link && <Link to={link}>Click here</Link>}
       </div>
     );
   }
