133471837a0ac830fad1c1bbd936529738455001
{Ben Cannon} <{BCannon1@sheffield.ac.uk}>
2024-05-25 20:43:20 +0100

    Added links to description boxes

--- ben-cannon-portfolio/src/components/DescriptionBox.js
+++ ben-cannon-portfolio/src/components/DescriptionBox.js
@@ -3,7 +3,7 @@ function DescriptionBox({title, content, link}) {
       <div>
         <h2 className='text-3xl font-bold font-mono'>{title}</h2>
         <p className='text-sm font-mono'>{content}</p>
-        {link && <a href={link} rel="noopener noreferrer" target="_blank">click here</a>}
+        {link && <a href={link}>click here</a>}
       </div>
     );
   }
--- ben-cannon-portfolio/src/pages/HomePage.js
+++ ben-cannon-portfolio/src/pages/HomePage.js
@@ -4,10 +4,10 @@ import GridLayout from "../components/GridLayout";
 function HomePage() {
   const data = [<DescriptionBox title="about me." content="Hi my name is Ben Cannon. I'm a Computer Science student at the University of Sheffield."/>, 
   <div><a href="https://www.linkedin.com/in/ben-cannon04" rel="noopener noreferrer" target="_blank">linkedin</a></div>,
-  <DescriptionBox title="work"/>,
+  <DescriptionBox title="work" link="/Portfolio/work"/>,
   <DescriptionBox title="dark mode toggle"/>, 
-  <DescriptionBox title="extracurricular"/>,
-  <DescriptionBox title="university"/>,
+  <DescriptionBox title="extracurricular" link="/Portfolio/extracurricular"/>,
+  <DescriptionBox title="university" link="/Portfolio/uni"/>,
   <DescriptionBox />]
 
   return (
