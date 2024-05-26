0b67194f4dccc9b7f861658be9e2d3a4878322af
{Ben Cannon} <{BCannon1@sheffield.ac.uk}>
2024-05-26 15:24:27 +0100

    Imported material UI and created Icon box

---
+++ ben-cannon-portfolio/src/components/IconBox.js
@@ -0,0 +1,10 @@
+function IconBox({icon, link}) {
+    return (
+      <div className="text-9xl cursor-pointer" onClick={(() => link && window.open(link, '_blank'))}>
+        {icon}
+      </div>
+    );
+  }
+  
+  export default IconBox;
+  
--- ben-cannon-portfolio/src/pages/HomePage.js
+++ ben-cannon-portfolio/src/pages/HomePage.js
@@ -1,9 +1,11 @@
 import DescriptionBox from "../components/DescriptionBox";
 import GridLayout from "../components/GridLayout";
+import IconBox from "../components/IconBox";
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
 
 function HomePage() {
   const data = [<DescriptionBox title="about me." content="Hi my name is Ben Cannon. I'm a Computer Science student at the University of Sheffield."/>, 
-  <div><a href="https://www.linkedin.com/in/ben-cannon04" rel="noopener noreferrer" target="_blank">linkedin</a></div>,
+  <IconBox link="https://www.linkedin.com/in/ben-cannon04" icon={<LinkedInIcon fontSize="inherit"/>}/>,
   <DescriptionBox title="work" link="/Portfolio/work"/>,
   <DescriptionBox title="dark mode toggle"/>, 
   <DescriptionBox title="extracurricular" link="/Portfolio/extracurricular"/>,
