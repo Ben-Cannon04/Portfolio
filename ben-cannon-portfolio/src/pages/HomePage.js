a56decb89c4d99c639c4e108bf8c8abf5f79b5d0
{Ben Cannon} <{BCannon1@sheffield.ac.uk}>
2024-05-26 15:31:10 +0100

    Added dark and light mode icons

--- ben-cannon-portfolio/src/pages/HomePage.js
+++ ben-cannon-portfolio/src/pages/HomePage.js
@@ -2,12 +2,14 @@ import DescriptionBox from "../components/DescriptionBox";
 import GridLayout from "../components/GridLayout";
 import IconBox from "../components/IconBox";
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import LightModeIcon from '@mui/icons-material/LightMode';
+import DarkModeIcon from '@mui/icons-material/DarkMode';
 
 function HomePage() {
   const data = [<DescriptionBox title="about me." content="Hi my name is Ben Cannon. I'm a Computer Science student at the University of Sheffield."/>, 
   <IconBox link="https://www.linkedin.com/in/ben-cannon04" icon={<LinkedInIcon fontSize="inherit"/>}/>,
   <DescriptionBox title="work" link="/Portfolio/work"/>,
-  <DescriptionBox title="dark mode toggle"/>, 
+  <IconBox icon={<DarkModeIcon fontSize="inherit"/>}/>, 
   <DescriptionBox title="extracurricular" link="/Portfolio/extracurricular"/>,
   <DescriptionBox title="university" link="/Portfolio/uni"/>,
   <DescriptionBox />]
