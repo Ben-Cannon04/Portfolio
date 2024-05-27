63a343894d0811be744063d3333ea5acc0f280b4
{Ben Cannon} <{BCannon1@sheffield.ac.uk}>
2024-05-27 20:06:46 +0100

    Updated navbar colour and look

--- ben-cannon-portfolio/src/components/NavBar.js
+++ ben-cannon-portfolio/src/components/NavBar.js
@@ -2,7 +2,7 @@ import { Link } from 'react-router-dom';
 
 function NavBar() {
     return (
-        <nav className="bg-slate-800 w-full top-0 shadow-md fixed">
+        <nav className="bg-sky-800 w-full top-0 shadow-md fixed rounded-md">
             <div className="container mx-auto px-4 py-2 text-gray-100">
                 <ul className="flex space-x-4 text-xs sm:text-base text-center whitespace-nowrap justify-center">
                     <li><Link to="/Portfolio/"><h2>Home</h2></Link></li>
