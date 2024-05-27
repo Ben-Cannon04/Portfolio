d9f0147afb9766bc92bac76c8c2e2c5b34b74e06
{Ben Cannon} <{BCannon1@sheffield.ac.uk}>
2024-05-27 15:59:12 +0100

    Navbar text size now responsive

--- ben-cannon-portfolio/src/components/NavBar.js
+++ ben-cannon-portfolio/src/components/NavBar.js
@@ -4,12 +4,12 @@ function NavBar() {
     return (
         <nav className="bg-slate-800 w-full top-0 shadow-md fixed">
             <div className="container mx-auto px-4 py-2 text-gray-100">
-                <ul className="flex space-x-4">
-                <li><Link to="/Portfolio/">Home</Link></li>
-                <li><Link to="/Portfolio/work">Work</Link></li>
-                <li><Link to="/Portfolio/uni">University</Link></li>
-                <li><Link to="/Portfolio/extracurricular">Extracurricular</Link></li>
-                <li><Link to="/Portfolio/creation">Portfolio Creation</Link></li>
+                <ul className="flex space-x-4 text-xs sm:text-base">
+                    <li><Link to="/Portfolio/"><h2>Home</h2></Link></li>
+                    <li><Link to="/Portfolio/work">Work</Link></li>
+                    <li><Link to="/Portfolio/uni">University</Link></li>
+                    <li><Link to="/Portfolio/extracurricular">Extracurricular</Link></li>
+                    <li><Link to="/Portfolio/creation">Portfolio Creation</Link></li>
                 </ul>
             </div>
         </nav>
