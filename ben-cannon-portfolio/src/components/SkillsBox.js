46e9a9d93ee8d745403a8cb41f4a945aaefcc7aa
{Ben Cannon} <74141617+Ben-Cannon04@users.noreply.github.com>
2025-07-19 14:54:57 +0100

    Created skill component

--- ben-cannon-portfolio/src/components/SkillsBox.js
+++ ben-cannon-portfolio/src/components/SkillsBox.js
@@ -1,4 +1,5 @@
 import PropTypes from 'prop-types';
+import Skill from './Skill';
 
 SkillsBox.propTypes = {
   title: PropTypes.string.isRequired,
@@ -8,21 +9,14 @@ SkillsBox.propTypes = {
 
 function SkillsBox({ title, content, skills }) {
   return (
-    <div className="text-pretty">
-      <h2 className="lg:text-3xl text-xl font-bold font-mono pt-4">{title}</h2>
+    <div className="text-pretty border border-border-light rounded-xl sm:rounded-2xl m-3 p-4 text-left bg-bg-light">
+      <h2 className="text-sm sm:text-base font-bold font-mono pt-4">{title}</h2>
 
       <p className="lg:text-sm text-xs font-mono pb-1">{content}</p>
 
-      <div className="flex flex-wrap gap-1 sm:gap-2 justify-center">
+      <div className="flex flex-wrap gap-1 sm:gap-2 ">
         {skills?.map((skill, index) => (
-          <span
-            key={index}
-            className="bg-gray-100 dark:bg-gray-700 text-gray-800 dark:text-gray-200 
-                       rounded-full px-3 sm:px-4 py-1 text-xs sm:text-sm font-medium 
-                       shadow-sm hover:shadow-md transition hover:scale-105"
-          >
-            {skill}
-          </span>
+          <Skill key={index} text={skill} />
         ))}
       </div>
     </div>
