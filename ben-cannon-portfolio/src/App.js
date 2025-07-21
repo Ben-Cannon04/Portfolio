3049980b1e7e819e961d8f64739da3be5058c89e
{Ben Cannon} <74141617+Ben-Cannon04@users.noreply.github.com>
2025-07-21 15:29:52 +0100

    Update App.js

--- ben-cannon-portfolio/src/App.js
+++ ben-cannon-portfolio/src/App.js
@@ -16,11 +16,23 @@ function App() {
       'via-white',
       'to-purple-50'
     );
-    
+
     if (isDarkMode) {
-      document.body.classList.add('min-h-screen', 'bg-gradient-to-br', 'from-gray-900', 'via-gray-800', 'to-gray-900');
+      document.body.classList.add(
+        'min-h-screen',
+        'bg-gradient-to-br',
+        'from-gray-900',
+        'via-gray-800',
+        'to-gray-900'
+      );
     } else {
-      document.body.classList.add('min-h-screen', 'bg-gradient-to-br', 'from-blue-50', 'via-white', 'to-purple-50');
+      document.body.classList.add(
+        'min-h-screen',
+        'bg-gradient-to-br',
+        'from-blue-50',
+        'via-white',
+        'to-purple-50'
+      );
     }
   }, [isDarkMode]);
 
@@ -28,8 +40,8 @@ function App() {
     <>
       <main
         className={`text-center py-2 min-h-screen ${
-          isDarkMode 
-            ? 'bg-gradient-to-br from-gray-900 via-gray-800 to-gray-900' 
+          isDarkMode
+            ? 'bg-gradient-to-br from-gray-900 via-gray-800 to-gray-900'
             : 'bg-gradient-to-br from-blue-50 via-white to-purple-50'
         }`}
       >
