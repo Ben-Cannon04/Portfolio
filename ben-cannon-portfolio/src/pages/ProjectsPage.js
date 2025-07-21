793367a03ec151c4cdeae5574b6a44eec9ed5abb
{Ben Cannon} <74141617+Ben-Cannon04@users.noreply.github.com>
2025-07-21 16:23:43 +0100

    Added back button to pages

--- ben-cannon-portfolio/src/pages/ProjectsPage.js
+++ ben-cannon-portfolio/src/pages/ProjectsPage.js
@@ -5,6 +5,7 @@ import {
   useComponentData,
 } from '../services/JsonToComponentConverterService';
 import Loading from '../components/Loading';
+import BackButton from '../components/BackButton';
 
 function ProjectsPage() {
   const [isDarkMode] = useOutletContext();
@@ -20,6 +21,10 @@ function ProjectsPage() {
 
   return (
     <Loading loading={loading} error={error} isDarkMode={isDarkMode}>
+      <div className="absolute sm:top-10 sm:left-10 top-5 left-5">
+        <BackButton isDarkMode={isDarkMode} />
+      </div>
+
       {data && (
         <GridLayout
           boxes={boxes}
