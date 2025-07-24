b027fb1272418e40f8cf5d99dc0141f62a11839a
{Ben Cannon} <74141617+Ben-Cannon04@users.noreply.github.com>
2025-07-24 14:54:16 +0100

    Added content to Technology Page

--- ben-cannon-portfolio/src/pages/TechnologiesPage.js
+++ ben-cannon-portfolio/src/pages/TechnologiesPage.js
@@ -1,9 +1,13 @@
 import { useOutletContext } from 'react-router-dom';
 import Loading from '../components/Loading';
 import TechBox from '../components/TechBox';
+import Filter from '../components/Filter';
+import { useState } from 'react';
 
 function TechnologiesPage() {
   const [isDarkMode] = useOutletContext();
+
+  const [filter, setFilter] = useState('all');
 
   return (
     <Loading isDarkMode={isDarkMode}>
@@ -16,6 +20,13 @@ function TechnologiesPage() {
               Software Technologies
             </h1>
           </div>
+
+          <Filter
+            filter={filter}
+            setFilter={setFilter}
+            filters={['all', 'C#', 'JavaScript', 'C++', 'Java']}
+            isDarkMode={isDarkMode}
+          />
 
           <div className="relative">
             <TechBox
@@ -35,13 +46,14 @@ function TechnologiesPage() {
                 },
               ]}
               proficiency="Advanced"
+              hide={filter != 'all' && filter != 'C#'}
               key={0}
             >
               <p>
-                I&apos;ve used C# in a series of personal project as well as in
-                industry .
+                I&apos;m expirenced in c# wpf, using it for 13 months in industry.
               </p>
             </TechBox>
+
             <TechBox
               title="Unity"
               subtitle="C#"
@@ -59,13 +71,108 @@ function TechnologiesPage() {
                 },
               ]}
               proficiency="Intermediate"
+              hide={filter != 'all' && filter != 'C#'}
               key={1}
             >
               <p>
-                I&apos;ve used C# in a series of personal project as well as in
-                industry .
+                I&apos;ve used unity in personal projects, school projects as well as at GameJams and Game development Society.
               </p>
             </TechBox>
+
+            <TechBox
+              title="React"
+              subtitle="JavaScript"
+              isDarkMode={isDarkMode}
+              projects={[
+                {
+                  text: 'Portfolio',
+                  to: '/Portfolio/projects',
+                  isClickable: true,
+                },
+                {
+                  text: 'University',
+                  to: '/Portfolio/education',
+                  isClickable: true,
+                },
+              ]}
+              proficiency="Intermediate"
+              hide={filter != 'all' && filter != 'JavaScript'}
+              key={2}
+            >
+              <p>
+                I&apos;ve used React in industry as well as at university and in personal projects. I&apos;m also learning react native with Expo in my own time currently.
+              </p>
+            </TechBox>
+
+            <TechBox
+              title="Core Java"
+              subtitle="Java"
+              isDarkMode={isDarkMode}
+              projects={[
+                {
+                  text: 'University',
+                  to: '/Portfolio/education',
+                  isClickable: true,
+                },
+                {
+                  text: 'Yunex Traffic',
+                  to: '/Portfolio/work',
+                  isClickable: true,
+                },
+              ]}
+              proficiency="Intermediate"
+              hide={filter != 'all' && filter != 'Java'}
+              key={3}
+            >
+              <p>
+                I&apos;ve used Java throughout university, create console application and Java Swing Applications. I&apos;ve also used it in industry as a backend with FFmpeg. 
+                In the future I would be interested in learning Java Spring Boot.
+              </p>
+            </TechBox>
+
+            <TechBox
+              title="TypeScript"
+              isDarkMode={isDarkMode}
+              projects={[
+                {
+                  text: 'Yunex Traffic',
+                  to: '/Portfolio/work',
+                  isClickable: true,
+                },
+              ]}
+              proficiency="Intermediate"
+              hide={filter != 'all' && filter != 'JavaScript'}
+              key={2}
+            >
+              <p>
+                I&apos;ve used TypeScript in industry. I&apos;m also using typescript in my own time
+              </p>
+            </TechBox>
+
+            <TechBox
+              title="C++"
+              isDarkMode={isDarkMode}
+              projects={[
+                {
+                  text: 'Simcyp Simulator V24',
+                  to: '/Portfolio/work',
+                  isClickable: true,
+                },
+                {
+                  text: 'Simcyp Simulator V25',
+                  to: '/Portfolio/work',
+                  isClickable: true,
+                },
+              ]}
+              proficiency="Beginner"
+              hide={filter != 'all' && filter != 'C++'}
+              key={4}
+            >
+              <p>
+                I&apos;ve used C++ in Industry for the backend of the Simcyp Simulator.
+              </p>
+            </TechBox>
+
             <TechBox
               title="ASP.Net"
               subtitle="C#"
@@ -78,11 +185,11 @@ function TechnologiesPage() {
                 },
               ]}
               proficiency="Beginner"
-              key={2}
+              hide={filter != 'all' && filter != 'C#'}
+              key={6}
             >
               <p>
-                I&apos;ve used C# in a series of personal project as well as in
-                industry .
+                I have a small  expierence with Asp.Net and would be intersed in furthering my knowledge.
               </p>
             </TechBox>
           </div>
