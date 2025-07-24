83c0a270cfaa9eb99cfd2d112419ee170d49fc95
{Ben Cannon} <74141617+Ben-Cannon04@users.noreply.github.com>
2025-07-24 13:49:57 +0100

    Updated skill so it can optionally have a link

--- ben-cannon-portfolio/src/components/Skill.js
+++ ben-cannon-portfolio/src/components/Skill.js
@@ -1,13 +1,36 @@
 import PropTypes from 'prop-types';
+import KeyboardArrowRightOutlinedIcon from '@mui/icons-material/KeyboardArrowRightOutlined';
+import { useNavigate } from 'react-router-dom';
 
 Skill.propTypes = {
   text: PropTypes.string.isRequired,
+  to: PropTypes.string,
+  isClickable: PropTypes.bool,
 };
 
-function Skill({ text }) {
+function Skill({ text, to, isClickable = false }) {
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    if (!isClickable) return;
+
+    navigate(to);
+  };
+
   return (
-    <span className="bg-blue-50 text-blue-700 px-2 sm:px-3 py-1 rounded-lg text-xs sm:text-sm cursor-default shadow-sm hover:shadow-md transition hover:scale-105">
+    <span
+      className={`px-2 sm:px-3 py-1 rounded-lg text-xs sm:text-sm transition flex items-center gap-1 
+        bg-blue-50 text-blue-700 hover:scale-105 shadow-sm hover:shadow-md ${
+        isClickable
+          ? 'cursor-pointer hover:bg-blue-100 active:bg-blue-200'
+          : ' cursor-default'
+      }`}
+      onClick={handleClick}
+    >
       {text}
+      {isClickable && (
+        <KeyboardArrowRightOutlinedIcon className="w-3 h-3 ml-1" />
+      )}
     </span>
   );
 }
